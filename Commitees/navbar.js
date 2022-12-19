88dbe94cc30df58f7e1f12c00df65415941d9c1c
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2022-12-20 00:54:00 +0530

    Add files via upload

--- Commitees/navbar.js
+++ Commitees/navbar.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         console.log(scroll);
-        if (scroll > 600) {
+        if (scroll > 00) {
             $(".navbar").css("background", "white");
             $(".nav-a").css("color", "black");
         }
